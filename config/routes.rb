8d41567e37581e7e18f7d6910336da8b5ec31bda
Sneha <81133168+snehabhamra@users.noreply.github.com>
2021-08-11 19:19:01 +1000

    updated routes so that log out link finally works, link is no longer broken

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :products
+  root to: 'store#index' 
   devise_for :users
-  root to: 'store#index'    
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/users/sign_out',  to: 'devise/sessions#destroy'
+  get '/users/sign_out' => :destroy, to: 'sessions#new'
 end
+
 
 
